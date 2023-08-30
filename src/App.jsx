8d17f9a61068a8e2a181a467e80a8248047381ed
Alaicry <alaicry99@gmail.com>
2023-08-30 21:58:52 +0500

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import ThemeSwitcher from "./components/ThemeSwitcher";
+
+const App = () => {
+	return (
+		<React.Fragment>
+			<Header />
+			<ThemeSwitcher />
+		</React.Fragment>
+	);
+};
+
+export default App;
